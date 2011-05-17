c136e73ff0595b83a0f2c2afe0584d30846cdd3d
Peter Berkenbosch <peter@pero-ict.nl>
2011-05-17 12:40:27 +0200

    try quick fixing a bug with variants that has no longer a ref to a product. product.touch renders error.

--- app/models/option_value_decorator.rb
+++ app/models/option_value_decorator.rb
@@ -4,6 +4,8 @@ OptionValue.class_eval do
   after_update :adjust_variant_prices, :if => :amount_changed?
 
   def adjust_variant_prices
-    variants.each{|v| v.update_attribute(:price, v.calculate_price)}
+    #first delete orphans.. not sure why they even exists..
+    variants.each {|v| v.destroy unless v.product}
+    variants.each {|v| v.update_attribute(:price, v.calculate_price)}
   end
 end
